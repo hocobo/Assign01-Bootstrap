918dacdf981125dd95f497387d46fad982cfcf40
hocobo <79883046+hocobo@users.noreply.github.com>
2023-02-02 11:14:44 -0600

    Completed requirements for assignment 02

--- js/client.js
+++ js/client.js
@@ -10,6 +10,15 @@ $(function(){
         toast.currentTime = 0;
         // play audio
         toast.play();
+        var product = $(this).attr('data-product');
+        var productCode = $(this).attr('data-code')
+        $("strong#product").text(product);
+        $("strong#code").text(productCode);
         $('#toast').toast({ autohide: false }).toast('show');
+    });
+    $(document).on('keydown', function(event) {
+        if (event.key == "Escape") {
+            $('#toast').toast({ autohide: true }).toast('hide');
+        }
     });
 });
